ef5096df30549d375f5dd586dbf4d9fb9af6f719
sudhanchitgopkar <sudhanchitgopkar@gmail.com>
2022-12-05 01:22:42 -0500

    edited: bresenham formatting

--- toolbox/line.cpp
+++ toolbox/line.cpp
@@ -9,35 +9,30 @@ void putPixel (SDL_Renderer* ren, int x, int y) {
 
 void bresLine (SDL_Renderer* ren, int x1, int y1, int x2, int y2) {
 
-  int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
+  
+  int dx = x2 - x1,dy = y2 - y1,dx1 = fabs(dx),dy1 = fabs(dy);
+  int px = 2 * dy1 - dx1,py = 2 * dx1 - dy1;
+  int x,y,xe,ye,i;
 
-  dx=x2-x1;
-  dy=y2-y1;
-  dx1=fabs(dx);
-  dy1=fabs(dy);
-  px=2*dy1-dx1;
-  py=2*dx1-dy1;
-
-  if(dy1<=dx1) {
-    if(dx>=0) {
+  if(dy1 <= dx1) {
+    if(dx >= 0) {
       x=x1;
       y=y1;
-      xe=x2;
+      xe=x2; 
     } else {
       x=x2;
       y=y2;
       xe=x1;
     } //if
     putPixel(ren,x,y);
-    for(i=0;x<xe;i++) {
-      x=x+1;
+    for(i=0;x++<xe;i++) {
       if(px<0)
 	px=px+2*dy1;
       else {
 	if((dx<0 && dy<0) || (dx>0 && dy>0))
-	  y=y+1;
+	  y++;
 	else
-	  y=y-1;
+	  y--;
 	px=px+2*(dy1-dx1);
       } //if
       putPixel(ren,x,y);
